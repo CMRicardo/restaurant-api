ac701ccea060393c84207bd54517b8b875efbc4d
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-11-08 13:17:04 -0600

    docs: :memo: Agregé la sección "dev" al README

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # API del Restaurante Brisas del Litoral
 
 API para el proyecto final de Programación e Implementación de Sistemas
+
+## Dev
+
+1. Levantar el servidor de desarrollo con 
+`npm start`.
+
+> Utilizamos `node --watch` que solo es compatible con la versión 18 o superior de node.
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import { createServer } from 'node:http'
 
-const server = createServer((req, res) => {
-  console.log('Request received!')
-  res.end('Hola mundo!')
-})
+const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  if (req.url === '/') {
+    res.end('Bienvenido a mi página!')
+  } else if (req.url === '/contact') {
+    res.end('Contacto!')
+  } else {
+    res.statusCode = 404
+    res.end('404 - Not Found!')
+  }
+}
+
+const server = createServer(processRequest)
 
 const PORT = process.env.PORT || 1234
 

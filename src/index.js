1e8f5956383731b5baa14988b1721c87590e9c51
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-11-08 18:30:18 -0600

    feat: :sparkles: Agregé el archivo customers.json para probar la API
    También añadí zod como dependencia, para validaciones

--- src/index.js
+++ src/index.js
@@ -1,25 +1,64 @@
-import { createServer } from 'node:http'
+import express from 'express'
+import crypto from 'node:crypto'
 
-const processRequest = (req, res) => {
-  const { method, url } = req
-  switch (method) {
-    case 'GET': {
-      switch (url) {
-        case '/':
-          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-          res.end(`Hola desde ${url}`)
-          break
+import customers from './customer.json' assert { type: 'json' }
 
-        default:
-          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-          res.end('404 - Not Found')
-          break
-      }
-    }
+const app = express()
+app.use(express.json())
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.send({message: 'Hola mundo'})
+})
+
+app.get('/customers', (req, res) => {
+  const { address } = req.query
+  if (address) {
+    const filteredCustomers = customers.filter(customer => customer.address === address)
+    return res.json(filteredCustomers)
   }
-}
-const server = createServer(processRequest)
+  res.json(customers)
+})
 
-server.listen(1234, () => {
-  console.log('Server listening on http://localhost:1234')
+app.get('/customers/:id', (req, res) => {
+  const { id } = req.params
+  const customer = customers.find(customer => customer.id === id)
+  if (customer) return res.send(customer)
+
+  res.status(404).send({message: '404 - Customer Not Found'})
+})
+
+app.post('/customers', (req, res) => {
+  const {
+    first_name,
+    second_name,
+    last_name,
+    email,
+    password,
+    address,
+    phone_number
+  } = req.body
+
+  const newCustomer = {
+    id: crypto.randomUUID(),
+    first_name,
+    second_name: second_name ?? '',
+    last_name,
+    email,
+    password,
+    address,
+    phone_number
+  }
+
+  customers.push(newCustomer)
+  res.status(201).json(newCustomer)
+})
+
+app.use((req, res) => {
+  res.status(404).send('404 - Not Found')
+})
+
+const PORT = process.env.PORT || 1234
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`)
 })

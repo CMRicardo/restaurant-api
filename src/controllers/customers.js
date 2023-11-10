a9ba9216bfa2622d710880200628b8d5b2d98723
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-11-10 17:10:44 -0600

    refactor: :recycle: Implementé el CustomerController

---
+++ src/controllers/customers.js
@@ -0,0 +1,51 @@
+import { CustomerModel } from '../models/customer.js'
+import { validateCustomer, validatePartialCustomer } from '../schemas/customer.js'
+
+export class CustomerController {
+  static async getAll (req, res) {
+    const { address } = req.query
+    const customers = await CustomerModel.getAll({ address })
+
+    res.json(customers)
+  }
+
+  static async getById (req, res) {
+    const { id } = req.params
+    const customer = await CustomerModel.getById({ id })
+    if (customer) return res.send(customer)
+
+    res.status(404).send({ message: '404 - Customer Not Found' })
+  }
+
+  static async create (req, res) {
+    const result = validateCustomer(req.body)
+
+    if (result.error) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const newCustomer = await CustomerModel.create({ input: result.data })
+    res.status(201).json(newCustomer)
+  }
+
+  static async delete (req, res) {
+    const { id } = req.params
+    const result = await CustomerModel.delete({ id })
+    if (!result) {
+      return res.status(404).json({ message: 'Customer Not Found!' })
+    }
+    return res.json({ message: 'Customer deleted!' })
+  }
+
+  static async update (req, res) {
+    const result = validatePartialCustomer(req.body)
+
+    if (result.error) return res.status(400).json(result.error.message)
+
+    const { id } = req.params
+
+    const updatedCustomer = await CustomerModel.update({ id, input: result.data })
+
+    res.json(updatedCustomer)
+  }
+}
--- src/routes/customers.js
+++ src/routes/customers.js
@@ -1,52 +1,15 @@
 import { Router } from 'express'
 
-import { validateCustomer, validatePartialCustomer } from '../schemas/customer.js'
-import { CustomerModel } from '../models/customer.js'
+import { CustomerController } from '../controllers/customers.js'
 
 export const customerRouter = Router()
 
-customerRouter.get('/', async (req, res) => {
-  const { address } = req.query
-  const customers = await CustomerModel.getAll({ address })
-  res.json(customers)
-})
+customerRouter.get('/', CustomerController.getAll)
 
-customerRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const customer = await CustomerModel.getById({ id })
-  if (customer) return res.send(customer)
+customerRouter.get('/:id', CustomerController.getById)
 
-  res.status(404).send({ message: '404 - Customer Not Found' })
-})
+customerRouter.delete('/:id', CustomerController.delete)
 
-customerRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-  const result = await CustomerModel.delete({ id })
-  if (!result) {
-    return res.status(404).json({ message: 'Customer Not Found!' })
-  }
-  return res.json({ message: 'Customer deleted!' })
-})
+customerRouter.post('/', CustomerController.create)
 
-customerRouter.post('/', async (req, res) => {
-  const result = validateCustomer(req.body)
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newCustomer = await CustomerModel.create({ input: result.data })
-  res.status(201).json(newCustomer)
-})
-
-customerRouter.patch('/:id', async (req, res) => {
-  const result = validatePartialCustomer(req.body)
-
-  if (result.error) return res.status(400).json(result.error.message)
-
-  const { id } = req.params
-
-  const updatedCustomer = await CustomerModel.update({ id, input: result.data })
-
-  res.json(updatedCustomer)
-})
+customerRouter.patch('/:id', CustomerController.update)

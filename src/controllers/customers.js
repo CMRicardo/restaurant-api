e6d1d389dd4b7eafbeadef1f022d1c9d89d92548
Ricardo Corrales <ricardocorrales84@gmail.com>
2023-11-11 09:40:38 -0600

    refactor: :recycle: Moví el CustomerModel a la carpeta local-file-system
    Esto para luego poder cambiarla por la base de datos real

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,4 +1,4 @@
-import { CustomerModel } from '../models/customer.js'
+import { CustomerModel } from '../models/local-file-system/customer.js'
 import { validateCustomer, validatePartialCustomer } from '../schemas/customer.js'
 
 export class CustomerController {
